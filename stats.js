bf4b1c2a028a6794d5b4d60b2730d91a79fa28fc
Rachel Scalfani <rscalfani@gmail.com>
2014-06-09 15:15:59 -0700

    displayed data with padding and minor code improvements

--- stats.js
+++ stats.js
@@ -1,13 +1,66 @@
+var deepcopy = require('deepcopy');
+
 module.exports = function(moduleOrder, logger, logFreq) {
 	var private = {
 		loggingId: null,
 		monitorCounters: {}, // moduleName: countersDef
+		getLongestLength: function(list) {
+				var longestLength = 0;
+				list.forEach(function(item) {
+					if (item && item.length > longestLength)
+						longestLength = item.length;
+				});
+				return longestLength;
+		},
+		padRight: function(str, length) {
+			str = str || '';
+			var numberOfSpaces = length - str.length;
+			for (var i = 0; i < numberOfSpaces; ++i)
+				str = str + ' ';
+			return str;
+		},
+		padLeft: function(str, length) {
+			str = str || '';
+			var numberOfSpaces = length - str.length;
+			for (var i = 0; i < numberOfSpaces; ++i)
+				str = ' ' + str;
+			return str;
+		},
 		formatCounters: function() {
+			var formatted = '';
+			moduleOrder.forEach(function(moduleName) {
+				var countersDef = private.monitorCounters[moduleName]
+				if (countersDef)
+				{
+					formatted += 'Module: ' + moduleName + '\n';
+					var list = function(key) {
+						return Object.keys(countersDef).map(function(counterName) {
+							return countersDef[counterName][key];
+						});
+					};
+					var mapDate = function(ticks) {
+						if (ticks)
+							return '[' + new Date(ticks) + ']';
+					};
+					var longestDisplayName = private.getLongestLength(list('displayName')) + 10;
+					var longestCounter = private.getLongestLength(list('counter').map(String)) + 10;
+					var longestLastUpdated = private.getLongestLength(list('lastUpdated').map(mapDate)) + 10;
 
+					Object.keys(countersDef).forEach(function(counterName) {
+						var displayName = private.padRight(countersDef[counterName].displayName, longestDisplayName);
+						var counter = private.padLeft(String(countersDef[counterName].counter), longestCounter);
+						var lastUpdated = private.padLeft(mapDate(countersDef[counterName].lastUpdated), longestLastUpdated);
+						formatted += displayName + counter + (lastUpdated || '') + '\n';
+					});
+				}
+			});
+			// return big string to be logged
+			return formatted;
 		}
 	};
 	var stats = {
 		registerCounters: function(moduleName, countersDef) {
+			countersDef = deepcopy(countersDef);
 			// saves countersDef for specified module
 			private.monitorCounters[moduleName] = countersDef;
 			// add and initialize counters
@@ -18,6 +71,7 @@ module.exports = function(moduleOrder, logger, logFreq) {
 			return {
 				increment: function(counterName){
 					++countersDef[counterName].counter;
+					countersDef[counterName].lastUpdated = Date.now();
 				},
 				reset: function(counterName){
 					countersDef[counterName].counter = 0;
@@ -27,8 +81,9 @@ module.exports = function(moduleOrder, logger, logFreq) {
 		startLogging: function() {
 			if (!private.loggingId)
 			{
+				logger.info(private.formatCounters()); // TODO remove after testing
 				private.loggingId = setInterval(function(){
-					logger.info(private.monitorCounters);
+					logger.info(private.formatCounters());
 				}, logFreq * 1000);
 			}
 		},
