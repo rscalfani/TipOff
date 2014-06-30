65a2786c2fee2fb495f1422f7eefc6a63443f4ac
Rachel Scalfani <rscalfani@gmail.com>
2014-06-30 13:01:09 -0700

    factored out formatting code

--- stats.js
+++ stats.js
@@ -1,56 +1,11 @@
 var deepcopy = require('deepcopy');
 
-module.exports = function(moduleOrder, loggers, logFreq, timers) {
+module.exports = function(moduleOrder, loggers, logFreq, timers, formatter) {
 	var logger = loggers.stats;
 	var private = {
 		loggingId: null,
 		monitorCounters: {}, // moduleName: countersDef
 		websites: {}, // url: name
-		getLongestLength: function(list) {
-				var longestLength = 0;
-				list.forEach(function(item) {
-					if (item && item.length > longestLength)
-						longestLength = item.length;
-				});
-				return longestLength;
-		},
-		padRight: function(str, length, pad) {
-			str = str || '';
-			str = String(str);
-			pad = pad || ' ';
-			var numberOfSpaces = length - str.length;
-			for (var i = 0; i < numberOfSpaces; ++i)
-				str = str + pad;
-			return str;
-		},
-		padLeft: function(str, length, pad) {
-			str = str || '';
-			str = String(str);
-			pad = pad || ' ';
-			var numberOfSpaces = length - str.length;
-			for (var i = 0; i < numberOfSpaces; ++i)
-				str = pad + str;
-			return str;
-		},
-		createBuffer: function(length) {
-			// hack to convert an empty array to one with undefined values so we can use map
-			// ex: Array(3) --> [ , , ]
-			// ex: Array.apply(null, Array(3)) --> [undefined, undefined, undefined]
-			return Array.apply(null, Array(length)).map(function() {
-				return '';
-			});
-		},
-		addRepeatingColumn: function(buffer, column) {
-			buffer.forEach(function(item, index) {
-				buffer[index] += column;
-			});
-		},
-		addColumnToBuffer: function(buffer, column, columnPad, padFunc) {
-			var longestLength = private.getLongestLength(column) + columnPad;
-			column.forEach(function(item, index) {
-				buffer[index] += padFunc(item, longestLength);
-			});
-		},
 		formatCounters: function() {
 			var formatted = '';
 			moduleOrder.forEach(function(moduleName) {
@@ -58,21 +13,21 @@ module.exports = function(moduleOrder, loggers, logFreq, timers) {
 				if (countersDef)
 				{
 					formatted += 'Module: ' + moduleName + '\n';
-					var buffer = private.createBuffer(Object.keys(countersDef).length);
+					var buffer = formatter.createBuffer(Object.keys(countersDef).length);
 					var list = function(key) {
 						return Object.keys(countersDef).map(function(counterName) {
 							return countersDef[counterName][key];
 						});
 					};
-					private.addColumnToBuffer(buffer, list('displayName'), 10, private.padRight);
-					private.addColumnToBuffer(buffer, list('counter').map(String), 10, private.padLeft);
-					private.addRepeatingColumn(buffer, '          ');
-					private.addColumnToBuffer(buffer, list('lastUpdated').map(function(ticks) {
+					formatter.addColumnToBuffer(buffer, list('displayName'), 10, formatter.padRight);
+					formatter.addColumnToBuffer(buffer, list('counter').map(String), 10, formatter.padLeft);
+					formatter.addRepeatingColumn(buffer, '          ');
+					formatter.addColumnToBuffer(buffer, list('lastUpdated').map(function(ticks) {
 						if (ticks)
 							return '[' + new Date(ticks) + ']';
 						else
 							return '';
-					}), 10, private.padRight);
+					}), 10, formatter.padRight);
 					formatted += buffer.join('\n');
 				}
 			});
@@ -83,7 +38,7 @@ module.exports = function(moduleOrder, loggers, logFreq, timers) {
 			var formatted = '\n';
 			var websites = private.websites;
 			var urls = Object.keys(websites);
-			var buffer = private.createBuffer(urls.length);
+			var buffer = formatter.createBuffer(urls.length);
 			var names = urls.map(function(url) {
 				return websites[url];
 			});
@@ -102,7 +57,7 @@ module.exports = function(moduleOrder, loggers, logFreq, timers) {
 				var mins = Math.floor(secs / 60);
 				// secs = secs - (mins * how many secs in min) OR secs - mins in secs unit
 				secs -= mins * 60;
-				return days + ' days, ' + private.padLeft(hrs, 2, '0') + ':' + private.padLeft(mins, 2, '0') + ':' + private.padLeft(secs, 2, '0');
+				return days + ' days, ' + formatter.padLeft(hrs, 2, '0') + ':' + formatter.padLeft(mins, 2, '0') + ':' + formatter.padLeft(secs, 2, '0');
 			};
 			var getTimers = function(type) {
 				return urls.map(function(url) {
@@ -119,14 +74,14 @@ module.exports = function(moduleOrder, loggers, logFreq, timers) {
 					return Math.round((timers.getTimerValue(url, 'up') / totals[index]) * 100) + '%';
 				});
 			};
-			private.addColumnToBuffer(buffer, names, 5, private.padRight);
-			private.addRepeatingColumn(buffer, '   uptime:');
-			private.addColumnToBuffer(buffer, uptimes, 1, private.padLeft);
-			private.addRepeatingColumn(buffer, '        downtime:');
-			private.addColumnToBuffer(buffer, downtimes, 1, private.padLeft);
-			private.addRepeatingColumn(buffer, '        (up:');
-			private.addColumnToBuffer(buffer, getPercentages(), 1, private.padLeft);
-			private.addRepeatingColumn(buffer, ')');
+			formatter.addColumnToBuffer(buffer, names, 5, formatter.padRight);
+			formatter.addRepeatingColumn(buffer, '   uptime:');
+			formatter.addColumnToBuffer(buffer, uptimes, 1, formatter.padLeft);
+			formatter.addRepeatingColumn(buffer, '        downtime:');
+			formatter.addColumnToBuffer(buffer, downtimes, 1, formatter.padLeft);
+			formatter.addRepeatingColumn(buffer, '        (up:');
+			formatter.addColumnToBuffer(buffer, getPercentages(), 1, formatter.padLeft);
+			formatter.addRepeatingColumn(buffer, ')');
 			formatted += buffer.join('\n');
 			return formatted;
 		}
