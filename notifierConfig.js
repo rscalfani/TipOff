476bf14d22704688bc22a32fd3acd8bc1873b1a2
Rachel Scalfani <rscalfani@gmail.com>
2014-06-30 17:05:54 -0700

    added nagIntervalTimerFreq and email settings

--- notifierConfig.js
+++ notifierConfig.js
@@ -1,5 +1,18 @@
 var config = {
-	intervalTimerFreq: 30 // in seconds
+	intervalTimerFreq: 30, // in seconds
+	nagIntervalTimerFreq: 60, // 3600,// in seconds
+	transport: {
+		host: 'mailtrap.io',
+		port: 465,
+		auth: {
+			user: '21426859fad676806',
+			pass: '01d93f1d839a96'
+		}
+	},
+	mailOptions: {
+		from: 'rscalfani@gmail.com',
+		to: 'rscalfani@gmail.com'
+	}
 };
 
 module.exports = config;
