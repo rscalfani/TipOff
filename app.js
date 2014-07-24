4081f356aecb275d37fd34b386f1de66e1d862d1
Rachel Scalfani <rscalfani@gmail.com>
2014-07-24 15:28:32 -0700

    require api and its config initialize and start api

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var loggers = {
 	op: log4js.getLogger('op'),
 	stats: log4js.getLogger('stats')
 };
+var apiConfig = require('./apiConfig');
+var api = require('./api')(loggers, apiConfig);
 var config = require('./config');
 var timers = require('./timers')(loggers);
 var formatter = require('./formatter')();
@@ -21,8 +23,9 @@ process.on('uncaughtException', function(err) {
 var exit = function(type) {
 	return function() {
 		loggers.op.info('exiting, received: ' + type);
-		stats.stop();
+		api.stop();
 		monitor.stop();
+		stats.stop();
 		notifier.stop();
 		log4js.shutdown(function() {
 			process.exit(1);
@@ -33,8 +36,14 @@ var exit = function(type) {
 process.on('SIGINT', exit('SIGINT'));
 process.on('SIGTERM', exit('SIGTERM'));
 
+api.start();
 monitor.init();
 monitor.start();
 stats.start();
 notifier.init();
 notifier.start();
+// app passes functions from monitor to api
+api.init({
+	startGrace: monitor.startGrace,
+	stopGrace: monitor.stopGrace
+});
