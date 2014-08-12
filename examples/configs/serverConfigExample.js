1047813f5f77cbc0e5a05af8d779842f65df1957
Rachel Scalfani <rscalfani@gmail.com>
2014-08-12 15:54:28 -0700

    created serverConfigExample.js

---
+++ examples/configs/serverConfigExample.js
@@ -0,0 +1,8 @@
+module.exports = {
+	pidFile: '/tmp/forever.pid',
+	logFile: '/tmp/forever.log',
+	outFile: '/tmp/forever.out',
+	errFile: '/tmp/forever.err',
+	watch: true,
+	watchDirectory: '/tmp/TipOff.watch'
+};
