a51f942957311e1f78beaee5ec06650d5437471f
Rachel Scalfani <rscalfani@gmail.com>
2014-08-12 15:15:15 -0700

    refactored code

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
+var _ = require('underscore');
+var serverConfig = require ('./configs/serverConfig');
+
 var forever = require('forever-monitor');
 var log4js = require('log4js');
 log4js.configure(require('./configs/loggingConfig'));
 var logger = log4js.getLogger('forever');
 
-var child = new (forever.Monitor)('app.js', {
+var child = new (forever.Monitor)('app.js', _.extend({
 	silent: false,
 	max: 100,
 	killTree: true,
 	killSignal: 'SIGTERM',
 	minUptime: 60000,
-	spinSleepTime: 5000,
-	pidFile: '/tmp/forever.pid',
-	logFile: '/tmp/forever.log',
-	outFile: '/tmp/forever.out',
-	errFile: '/tmp/forever.err',
-	watch: true,
-	watchDirectory: '/tmp/TipOff.watch'
-});
+	spinSleepTime: 5000
+}, serverConfig));
 
 child.on('restart', function() {
 	logger.error('app is restarting');
