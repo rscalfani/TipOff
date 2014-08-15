fe0b6b9470295b21c22bd5e7cb4254935a63ecac
Rachel Scalfani <rscalfani@gmail.com>
2014-08-15 12:56:03 -0700

    added startUp function and copy configs into config folder

--- app.js
+++ app.js
@@ -1,49 +1,75 @@
-var log4js = require('log4js');
-log4js.configure(require('./configs/loggingConfig'));
-var loggers = {
-	op: log4js.getLogger('op'),
-	stats: log4js.getLogger('stats')
-};
-var apiConfig = require('./configs/apiConfig');
-var api = require('./api')(loggers, apiConfig);
-var config = require('./configs/config');
-var timers = require('./timers')(loggers);
-var formatter = require('./formatter')();
-var stats = require('./stats')(['monitor', 'notifier'], loggers, config.statsFreq, timers, formatter);
-var monitorConfig = require('./configs/monitorConfig');
-var monitor = require('./monitor')(loggers, stats, monitorConfig);
-var notifierConfig = require('./configs/notifierConfig');
-var notifier = require('./notifier')(loggers, formatter, stats, monitor, notifierConfig);
-
-process.on('uncaughtException', function(err) {
-	loggers.op.fatal('uncaught exception: ' + err.stack);
-	process.exit(1);
-});
-
-var exit = function(type) {
-	return function() {
-		loggers.op.info('exiting, received: ' + type);
-		api.stop();
-		monitor.stop();
-		stats.stop();
-		notifier.stop();
-		log4js.shutdown(function() {
-			process.exit(1);
-		});
+var startUp = function() {
+	var log4js = require('log4js');
+	log4js.configure(require('./configs/loggingConfig'));
+	var loggers = {
+		op: log4js.getLogger('op'),
+		stats: log4js.getLogger('stats')
 	};
+	var apiConfig = require('./configs/apiConfig');
+	var api = require('./api')(loggers, apiConfig);
+	var config = require('./configs/config');
+	var timers = require('./timers')(loggers);
+	var formatter = require('./formatter')();
+	var stats = require('./stats')(['monitor', 'notifier'], loggers, config.statsFreq, timers, formatter);
+	var monitorConfig = require('./configs/monitorConfig');
+	var monitor = require('./monitor')(loggers, stats, monitorConfig);
+	var notifierConfig = require('./configs/notifierConfig');
+	var notifier = require('./notifier')(loggers, formatter, stats, monitor, notifierConfig);
+
+	process.on('uncaughtException', function (err) {
+		loggers.op.fatal('uncaught exception: ' + err.stack);
+		process.exit(1);
+	});
+
+	var exit = function (type) {
+		return function () {
+			loggers.op.info('exiting, received: ' + type);
+			api.stop();
+			monitor.stop();
+			stats.stop();
+			notifier.stop();
+			log4js.shutdown(function () {
+				process.exit(1);
+			});
+		};
+	};
+
+	process.on('SIGINT', exit('SIGINT'));
+	process.on('SIGTERM', exit('SIGTERM'));
+
+	api.start();
+	monitor.init();
+	monitor.start();
+	stats.start();
+	notifier.init();
+	notifier.start();
+	// app passes functions from monitor to api
+	api.init({
+		startGrace: monitor.startGrace,
+		stopGrace: monitor.stopGrace
+	});
 };
 
-process.on('SIGINT', exit('SIGINT'));
-process.on('SIGTERM', exit('SIGTERM'));
-
-api.start();
-monitor.init();
-monitor.start();
-stats.start();
-notifier.init();
-notifier.start();
-// app passes functions from monitor to api
-api.init({
-	startGrace: monitor.startGrace,
-	stopGrace: monitor.stopGrace
-});
+var argv = require('optimist')
+	.usage('Usage:\n$0 --configPath <path>')
+	.boolean('forever')
+	.string('configPath')
+	.describe('configPath', 'path to directory that contains only TipOff config files')
+	.argv;
+if (argv.forever)
+	startUp();
+else
+{
+	var fs = require('fs');
+	if (!fs.existsSync('configs'))
+		fs.mkdirSync('configs', 0755);
+	var exec = require('child_process').exec;
+	exec('cp ' + argv.configPath + '/*onfig.js configs/', function(err, stdout, stderr) {
+		if (err) {
+			console.log(stdout);
+			console.log(stderr);
+		}
+		else
+			startUp();
+	});
+}
--- server.js
+++ server.js
@@ -1,26 +1,47 @@
-var _ = require('underscore');
-var serverConfig = require ('./configs/serverConfig');
+var startUp = function() {
+	var _ = require('underscore');
+	var serverConfig = require ('./configs/serverConfig');
 
-var forever = require('forever-monitor');
-var log4js = require('log4js');
-log4js.configure(require('./configs/loggingConfig'));
-var logger = log4js.getLogger('forever');
+	var forever = require('forever-monitor');
+	var log4js = require('log4js');
+	log4js.configure(require('./configs/loggingConfig'));
+	var logger = log4js.getLogger('forever');
 
-var child = new (forever.Monitor)('app.js', _.extend({
-	silent: false,
-	max: 100,
-	killTree: true,
-	killSignal: 'SIGTERM',
-	minUptime: 60000,
-	spinSleepTime: 5000
-}, serverConfig));
+	var child = new (forever.Monitor)('app.js', _.extend({
+		silent: false,
+		max: 100,
+		killTree: true,
+		killSignal: 'SIGTERM',
+		minUptime: 60000,
+		spinSleepTime: 5000,
+		options: ['--forever']
+	}, serverConfig));
 
-child.on('restart', function() {
-	logger.error('app is restarting');
+	child.on('restart', function() {
+		logger.error('app is restarting');
+	});
+
+	child.on('exit', function() {
+		logger.error('app has exited after ' + child.max + ' number of tries');
+	});
+
+	child.start();
+};
+
+var argv = require('optimist')
+	.usage('Usage:\n$0 --configPath <path>')
+	.string('configPath')
+	.describe('configPath', 'path to directory that contains only TipOff config files')
+	.argv;
+var fs = require('fs');
+if (!fs.existsSync('configs'))
+	fs.mkdirSync('configs', 0755);
+var exec = require('child_process').exec;
+exec('cp ' + argv.configPath + '/*onfig.js configs/', function(err, stdout, stderr) {
+	if (err) {
+		console.log(stdout);
+		console.log(stderr);
+	}
+	else
+		startUp();
 });
-
-child.on('exit', function() {
-	logger.error('app has exited after ' + child.max + ' number of tries');
-});
-
-child.start();
